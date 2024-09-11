2d66ad54d37c264b766f7ec1f2eb4eec824e970a
nyepes2 <nyepes2@fa24-cs425-0601.cs.illinois.edu>
2024-09-10 19:52:12 -0500

    Code

---
+++ mp1/server.py
@@ -0,0 +1,49 @@
+from constants import HOSTS, PORT, MAX_CLIENTS
+import socket
+import threading
+import subprocess
+from shared import send_data, receive_data
+
+def run_query(query: str):
+    """
+    Runs the arguments for the query passed in.
+    Returns the string containing the result.
+    """
+    command = f"grep {query} machine.i.log"
+    # Runs the command given and captures output so that the result can be retrieved.
+    result = subprocess.run(command, capture_output=True, text = True, shell=True)
+    output_value = result.stdout.strip()
+    return output_value
+
+def handle_client(client_socket: socket.socket):
+    """
+    Given a socket connection to the client, this method will run the query given by the user
+    to the machine's log file and return the result back to the client.
+    """
+    command = receive_data(client_socket)
+    result = run_query(command)
+    send_data(client_socket, result)
+    client_socket.close()
+
+def start_server():
+    """
+    Create a server listening on PORT on its address
+    Constantly waiting on new connection, to execute handle client.
+    """
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Lets server reuse address so that it can relaunch quickly
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # TODO: Change when we get all hosts
+    server.bind((HOSTS[0], PORT))
+    server.listen(MAX_CLIENTS)
+    
+    while True:
+        client_socket, _ = server.accept()
+        # Creates a new thread for each client
+        client_handler = threading.Thread(target=handle_client, args=(client_socket))
+        # sets daemon to true so that there is no need of joining threads once thread finishes
+        client_handler.daemon = True
+        client_handler.start()
+
+if __name__ == "__main__":
+    start_server()
---
+++ mp1/src/client.py
@@ -0,0 +1,42 @@
+from shared.constants import HOSTS, PORT
+import socket
+import sys
+from shared.shared import send_data, receive_data
+
+def query_host(host: str, arguments: str):
+    """
+    Opens up a connection with server to send the command passed in to arguments
+    and returns the result.
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        result = ""
+        server.connect((host, PORT))
+        send_data(server, arguments)
+        result = receive_data(server)
+    return result
+
+def query_all_hosts(arguments: str):
+    """
+    For each host query the command and print the result
+    """
+    for host in HOSTS:
+        data = query_host(host, arguments)
+        print(data)
+
+
+if __name__ == "__main__":
+    """
+    Application starting point.
+    Parses arguments and runs command
+    """
+    arguments = ''
+    # Skip the command name argument
+    for string in sys.argv[1:]:
+        # If argument has a space, put in quotations to not change meaning 
+        # (e.g. grep Hello World != grep "Hello World")
+        if (" " in string or ';' in string or '|' in string or '&' in string or ">" in string or "<" in string):
+            arguments += f'"{string}"'
+        else:
+            arguments += string
+        arguments += ' '
+    query_all_hosts(arguments)
