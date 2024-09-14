0db539151ad2cef1b5152c438a77aec9125c27bc
nyepes2 <nyepes2@fa24-cs425-0602.cs.illinois.edu>
2024-09-14 13:50:21 -0500

    git ignore

--- mp1/src/shared/constants.py
+++ mp1/src/shared/constants.py
@@ -4,11 +4,11 @@ HOSTS = [
     "fa24-cs425-0603.cs.illinois.edu",
     "fa24-cs425-0604.cs.illinois.edu",
     "fa24-cs425-0605.cs.illinois.edu",
-    "fa24-cs425-0606.cs.illinois.edu",
-    "fa24-cs425-0607.cs.illinois.edu",
-    "fa24-cs425-0608.cs.illinois.edu",
-    "fa24-cs425-0609.cs.illinois.edu",
-    "fa24-cs425-0610.cs.illinois.edu"
+    # "fa24-cs425-0606.cs.illinois.edu",
+    # "fa24-cs425-0607.cs.illinois.edu",
+    # "fa24-cs425-0608.cs.illinois.edu",
+    # "fa24-cs425-0609.cs.illinois.edu",
+    # "fa24-cs425-0610.cs.illinois.edu"
 ]
 
 PORT = 12345
