9d7793e931b200b528e53f84510b7b4af94836bb
nyepes2 <nyepes2@fa24-cs425-0601.cs.illinois.edu>
2024-12-07 18:46:03 -0600

    fs fix

--- mp4_test.sh
+++ mp4_test.sh
@@ -1,10 +1,10 @@
 ./run.sh create tests/TrafficSigns_1000 TrafficSigns_1000
 # sleep 1
 # ./run.sh create tests/TrafficSigns_5000 TrafficSigns_5000
-# sleep 1
+sleep 1
 
 # ./run.sh create tests/TrafficSigns_10000 TrafficSigns_10000
-sleep 2
+# sleep 2
 
 ./run.sh Rainstorm "python tests/mp4_demo1.py Unpunched" "python tests/mp4_demo2.py" TrafficSigns_1000 output 1
 # ./run.sh merge output
