f3321375c7545ba3256cd7d870cfd6371346558c
ilk2 <ilk2@fa24-cs425-0606.cs.illinois.edu>
2024-12-08 11:58:35 -0600

    advancements

--- tests/app2_op2.py
+++ tests/app2_op2.py
@@ -5,13 +5,15 @@ import sys
 machine_id = sys.argv[1]
 counts = dict()
 
+def init(start_state):
+    global counts
+    counts = start_state
+
 def count_by_category(key, value):
+    
+    row = value.split(",")
+    category = row[8]
+    counts[category] = counts.get(category, 0) + 1
+    return [(category, counts[category])]
 
-    if (key != "STATE"):
-
-        row = value.split(",")
-        category = row[8]
-        counts[category] = counts.get(category, 0) + 1
-        return [(category, counts[category])]
-
-rain_storm_framework(machine_id, count_by_category)
+rain_storm_framework(machine_id, count_by_category, init)
